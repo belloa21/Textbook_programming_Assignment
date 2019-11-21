50d71270331b7a825eb129fa3ed40051ba941174
alexr <belloa@hartwick.edu>
2019-11-21 09:38:46 -0500

    Updated

--- Textbook_programming_class.py
+++ Textbook_programming_class.py
@@ -1,1 +1,20 @@
+#Alex Bello
+#11/20/2019
+#Defines the Book class and its functions
 
+
+from Textbook_programming_person_class import Person
+
+
+class Textbook:
+    def __init__(self, author, edition, isbn_number, publisher, year_published, quantity_in_inventory, price ):
+        self.author = Person
+        self.edition = edition
+        self.isbn_number = isbn_number
+        self.publisher = publisher
+        self.year_published = year_published
+        self.quantity_in_inventory = quantity_in_inventory
+        self.price = price
+
+    def add_to_inventory(self, quantity_in_inventory):
+        quantity_in_inventory +=
--- Textbook_programming_person_class.py
+++ Textbook_programming_person_class.py
@@ -1,1 +1,14 @@
+#Alex Bello
+#11/20/2019
+#Defines the Person class and its functions
+
+
+class Person:
+    def __init__(self,first_name,last_name,age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def description(self):
+        print(f"The author's first name is {self.first_name} and their last name is {self.last_name}. They are {self.age} years old. ")
 
